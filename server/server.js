d0cfbf53de49bd51706cefc93042101667f276eb
7FFF00 <t42please@gmail.com>
2020-02-21 20:20:28 -0800

    REBUILD proxy using axios, remove unnecessary dependencies

--- server/server.js
+++ server/server.js
@@ -1,23 +1,24 @@
+// require('newrelic');
 const express = require('express');
-const proxy = require('http-proxy-middleware');
+const axios = require('axios');
 
 const app = express();
+
 
 const dirPath = `${__dirname}/../public/dist/`;
 app.use(express.static(dirPath));
 
 const { routes } = require('../proxyconfig.json');
 
-for (route of routes) {
-  app.use(route.route,
-      proxy({
-          target: route.address,
-          pathRewrite: (path, req) => {
-              return path.split('/').slice(2).join('/');
-          }
-      })
-  );
-}
+app.get('/:component/:room_id', (req, res) => {
+  const { component, room_id } = req.params;
+  axios.get(`${routes[component].address}/${room_id}`)
+    .then(data => res.status(200).send(data.data))
+    .catch(e => {
+      console.log(e);
+      res.status(500).send('Error');
+    });
+});
 
 
 app.listen(3000, () => console.log('Listening on port: 3000'));
